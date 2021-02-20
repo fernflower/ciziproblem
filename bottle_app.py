438739fcf8191873fd52d78e42255582bbd96772
Inessa Vasilevskaya <ivasilev@redhat.com>
2021-02-20 23:37:27 +0100

    Refactor: be more destructive
    - Support for json contexts removed together with the contexts
    - Refactored structures for form_context and system_context
    - (Hopefully) improved readability

--- bottle_app.py
+++ bottle_app.py
@@ -17,8 +17,9 @@ SYSTEM_CONTEXT = ["name", "postal_address", "datova_schranka", "link", "authorit
 
 
 def get_form_context(filename):
-    """Returns a default context dict"""
-    data = {}
+    """Returns 2 dicts - a form context dict and a system context dict"""
+    form_context = {}
+    system_context = {}
     with open(filename, encoding='utf-8') as f:
         if filename.endswith('.yaml'):
             # context is a yaml file
@@ -28,39 +29,22 @@ def get_form_context(filename):
                 # transform system keys to __key__
                 document = context['document']
                 for key in [k for k in SYSTEM_CONTEXT if k in document]:
-                    data['__{}__'.format(key)] = document[key]
                     # flatten dict in case of declinations - pass a dict of declinationPAD
                     if key == "declination":
                         for pad, values in document["declination"].items():
-                            declination_key = "__declination{}".format(pad)
-                            data[declination_key] = {v["before"]: v["after"] for v in values}
+                            declination_key = "declination{}".format(pad)
+                            system_context[declination_key] = {v["before"]: v["after"] for v in values}
+                    else:
+                        system_context['__{}__'.format(key)] = document[key]
                 # process form fields
                 form_fields = document['form'].get('fields', [])
-                sorted_data = collections.OrderedDict()
                 for field in form_fields:
-                    data[field["name"]] = field.get("default", "")
-                    if field.get("type", "text") != "text":
-                        # XXX FIXME this will be refactored in the near future
-                        # need to pass control type to the frontend:
-                        # - date is specified by the presence of __name__date: True
-                        # - radio has the format __name__radio: choices
-                        specific_format = "__{}_{}".format(field["name"], field["type"])
-                        if field["type"] == "date":
-                            data[specific_format] = True
-                        else:
-                            data[specific_format] = field.get("choices", [])
+                    form_context[field['name']] = field
             except KeyError as err:
                 raise exc.ConfigError("{} raised when processing {}".format(err, filename))
         else:
-            # context is a json file
-            try:
-                data = json.loads(f.read())
-            except:
-                raise exc.ConfigError("Error parsing json config {}".format(filename))
-        # specific context dict post-processing
-        if data['__postal_address__'] == 'minvnitra_offices_chooser':
-            data['__chosen_office'] = None
-        return data
+            raise NotImplemented("Only yaml contexts are supported")
+        return form_context, system_context
 
 
 def get_offices_list():
@@ -95,30 +79,26 @@ env = jinja2.Environment(
 )
 
 
-def docform(context):
-    template = env.get_or_select_template('docform.tpl')
-    system_context = {}
+def _prepare_for_front(form_context):
+    #j2 template expects a straightforward structure of { elemname: {'value': .., 'input': ..} }
     context_to_pass = {}
-    for key, value in context.items():
-        if key.startswith('__'):
-            system_context[key] = value
-        else:
-            # check if specific type of input is required, if no given it will be text
-            elem = { 'value': value }
-            radio = '__{}_radio'.format(key)
-            date = '__{}_date'.format(key)
+    for name, field_data in form_context.items():
+        # check if specific type of input is required, if no given it will be text
+        elem = {'value': field_data.get("default", ""), 'input': "text"}
+        if field_data.get("type", "text") != "text":
+            if field_data["type"] == "radio":
+                elem["input"] = "radio"
+                elem["ids"] = field_data["choices"]
+            elif field_data["type"] == "date":
+                elem["input"] = "date"
+        context_to_pass[name] = elem
+    return context_to_pass
 
-            if radio in context:
-                elem['input'] = 'radio'
-                elem['ids'] = context[radio]
-            elif date in context:
-                elem['input'] = 'date'
-            else:
-                elem['input'] = 'text'
-            context_to_pass[key] = elem
 
+def docform(form_context, system_context):
+    template = env.get_or_select_template('docform.tpl')
+    context_to_pass = _prepare_for_front(form_context)
     return template.render(context=context_to_pass,
-                           name=context.get('__name__', "Application"),
                            system_context=system_context,
                            minvnitra_offices=get_offices_list())
 
@@ -131,32 +111,27 @@ def index():
 
 @route('/necinnost_Nin1')
 def necinnost():
-    context = get_form_context('necinnost_Nin1_context.yaml')
-    return docform(context)
+    return docform(*get_form_context('necinnost_Nin1_context.yaml'))
 
 
 @route('/rodne_cislo_application')
 def rodne_cislo():
-    context = get_form_context('rodne_cislo_context.yaml')
-    return docform(context)
+    return docform(*get_form_context('rodne_cislo_context.yaml'))
 
 
 @route('/historie_pobytu')
 def historie_pobytu():
-    context = get_form_context('historie_pobytu_context.yaml')
-    return docform(context)
+    return docform(*get_form_context('historie_pobytu_context.yaml'))
 
 
 @route('/potvrzeni_o_soucasnem_pobytu')
 def potvrzeni_soucasny_pobyt():
-    context = get_form_context('potvrzeni_o_soucasnem_pobytu_context.yaml')
-    return docform(context)
+    return docform(*get_form_context('potvrzeni_o_soucasnem_pobytu_context.yaml'))
 
 
 @route('/urychleni_rizeni')
 def urychleni_rizeni():
-    context = get_form_context('urychleni_rizeni_context.yaml')
-    return docform(context)
+    return docform(*get_form_context('urychleni_rizeni_context.yaml'))
 
 
 def get_office_by_name(name):
@@ -170,18 +145,8 @@ def get_office_address():
     return json.dumps(office or {})
 
 
-@route('/generate', method="POST")
-def generate():
-    data = request.forms
-    docx_template_name = TEMPLATE_MAP.get(data.get('__form__'), {}).get('template')
-    default_context_name = TEMPLATE_MAP.get(data.get('__form__'), {}).get('context')
-    if not docx_template_name or not default_context_name:
-        return
-    default_context = get_form_context(default_context_name)
-    # vet against default context keys
-    user_input_vetted = {k: v for k, v in data.iteritems() if k in default_context and v}
-    context = dict(default_context)
-    context.update(user_input_vetted)
+def _apply_post_processing_hacks(context):
+    "Hacks to convert data received from frontend to the expected form in docx templates"
     # transition from YYYY-MM-DD dates to expected DD.MM.YYYY
     for date_key in [k for k in context if context.get('__{}_date'.format(k))]:
         try:
@@ -193,9 +158,24 @@ def generate():
     if '__chosen_office' in context:
         context['chosen_office'] = get_office_by_name(
                 context.get('__chosen_office')) or get_office_by_name('Pracoviště Praha V.')
-    # pass declination dicts if any present in context
-    for key in [k for k in context if k.startswith('__declination')]:
-        context[key.lstrip('__')] = context[key]
+
+
+@route('/generate', method="POST")
+def generate():
+    data = request.forms
+    form_name = data.get('__form__')
+    docx_template_name = TEMPLATE_MAP.get(form_name, {}).get('template')
+    context_name = TEMPLATE_MAP.get(form_name, {}).get('context')
+    if not docx_template_name or not context_name:
+        raise ConfigError("No routing specified for {}".format(form_name))
+    form_context, system_context = get_form_context(context_name)
+    # vet against default context keys
+    allowed_keys = list(form_context.keys()) + ['__chosen_office']
+    user_input_vetted = {k: v for k, v in data.iteritems() if k in allowed_keys and v}
+    context = {v["name"]: v.get("default", "") for k, v in form_context.items()}
+    context.update(system_context)
+    context.update(user_input_vetted)
+    _apply_post_processing_hacks(context)
     with tempfile.NamedTemporaryFile(dir="generated", delete=True) as temp_doc:
         gen.generate_doc(docx_template_name, context, temp_doc.name)
         return static_file(
