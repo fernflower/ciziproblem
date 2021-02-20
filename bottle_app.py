c3b637bff552d7638178d384b765cfb4e1584002
Inessa Vasilevskaya <ivasilev@redhat.com>
2021-02-20 22:24:48 +0100

    Support yaml context
    Non-destructive change that adds support for yaml contexts and switches
    to using yaml contexts for all 5 applications.

--- bottle_app.py
+++ bottle_app.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import jinja2
 import json
@@ -7,19 +8,60 @@ import yaml
 
 from bottle import default_app, request, route, run, static_file
 
+import exc
 import generate as gen
 
 
-def _default_context(filename):
+# These are the fields that are not related to a specific form
+SYSTEM_CONTEXT = ["name", "postal_address", "datova_schranka", "link", "authority", "declination"]
+
+
+def get_form_context(filename):
     """Returns a default context dict"""
-    with open(filename) as f:
-        try:
-            data = json.loads(f.read())
-            if data['__postal_address__'] == 'minvnitra_offices_chooser':
-                data['__chosen_office'] = None
-            return data
-        except:
-            return {}
+    data = {}
+    with open(filename, encoding='utf-8') as f:
+        if filename.endswith('.yaml'):
+            # context is a yaml file
+            context = yaml.safe_load(f)
+            document = context.get('document', {})
+            try:
+                # transform system keys to __key__
+                document = context['document']
+                for key in [k for k in SYSTEM_CONTEXT if k in document]:
+                    data['__{}__'.format(key)] = document[key]
+                    # flatten dict in case of declinations - pass a dict of declinationPAD
+                    if key == "declination":
+                        for pad, values in document["declination"].items():
+                            declination_key = "__declination{}".format(pad)
+                            data[declination_key] = {v["before"]: v["after"] for v in values}
+                # process form fields
+                form_fields = document['form'].get('fields', [])
+                sorted_data = collections.OrderedDict()
+                for field in form_fields:
+                    data[field["name"]] = field.get("default", "")
+                    if field.get("type", "text") != "text":
+                        # XXX FIXME this will be refactored in the near future
+                        # need to pass control type to the frontend:
+                        # - date is specified by the presence of __name__date: True
+                        # - radio has the format __name__radio: choices
+                        specific_format = "__{}_{}".format(field["name"], field["type"])
+                        if field["type"] == "date":
+                            data[specific_format] = True
+                        else:
+                            data[specific_format] = field.get("choices", [])
+            except KeyError as err:
+                raise exc.ConfigError("{} raised when processing {}".format(err, filename))
+        else:
+            # context is a json file
+            try:
+                data = json.loads(f.read())
+            except:
+                raise exc.ConfigError("Error parsing json config {}".format(filename))
+        # specific context dict post-processing
+        if data['__postal_address__'] == 'minvnitra_offices_chooser':
+            data['__chosen_office'] = None
+        return data
+
 
 def get_offices_list():
     with open('minvnitra_offices', encoding='utf8') as f:
@@ -29,25 +71,22 @@ def get_offices_list():
 
 OFFICES = get_offices_list()
 TEMPLATE_MAP = {
-        "Trvalý pobyt: Žádost o uplatnění opatření proti nečinnosti": {
-            "template": "zadost_o_uplatneni_opatreni_proti_necinnosti_spravniho_organu.docx",
-            "context": "necinnost_trvaly_context"},
         "Žádost o přidělení rodného čísla": {
             "template": "zadost_rodne_cislo.docx",
-            "context": "rodne_cislo_context"},
+            "context": "rodne_cislo_context.yaml"},
         "Historie pobytu": {
             "template": "zadost_o_historie_pobytu.docx",
-            "context": "historie_pobytu_context"},
+            "context": "historie_pobytu_context.yaml"},
         "Potvrzení o současném pobytu": {
             "template": "zadost_potvrzeni_soucasneho_pobytu.docx",
-            "context": "potvrzeni_o_soucasnem_pobytu_context"},
+            "context": "potvrzeni_o_soucasnem_pobytu_context.yaml"},
         "Žádost o uplatnění opatření proti nečinnosti": {
             "template": "zadost_o_uplatneni_opatreni_proti_necinnosti_spravniho_organu_Nin1.docx",
-            "context": "necinnost_Nin1_context"
+            "context": "necinnost_Nin1_context.yaml"
             },
         "Žádost o urychlení řízení": {
             "template": "zadost_urychleni_rizeni.docx",
-            "context": "urychleni_rizeni_context"
+            "context": "urychleni_rizeni_context.yaml"
             }
         }
 env = jinja2.Environment(
@@ -92,31 +131,31 @@ def index():
 
 @route('/necinnost_Nin1')
 def necinnost():
-    context = _default_context('necinnost_Nin1_context')
+    context = get_form_context('necinnost_Nin1_context.yaml')
     return docform(context)
 
 
 @route('/rodne_cislo_application')
 def rodne_cislo():
-    context = _default_context('rodne_cislo_context')
+    context = get_form_context('rodne_cislo_context.yaml')
     return docform(context)
 
 
 @route('/historie_pobytu')
 def historie_pobytu():
-    context = _default_context('historie_pobytu_context')
+    context = get_form_context('historie_pobytu_context.yaml')
     return docform(context)
 
 
 @route('/potvrzeni_o_soucasnem_pobytu')
 def potvrzeni_soucasny_pobyt():
-    context = _default_context('potvrzeni_o_soucasnem_pobytu_context')
+    context = get_form_context('potvrzeni_o_soucasnem_pobytu_context.yaml')
     return docform(context)
 
 
 @route('/urychleni_rizeni')
 def urychleni_rizeni():
-    context = _default_context('urychleni_rizeni_context')
+    context = get_form_context('urychleni_rizeni_context.yaml')
     return docform(context)
 
 
@@ -138,7 +177,7 @@ def generate():
     default_context_name = TEMPLATE_MAP.get(data.get('__form__'), {}).get('context')
     if not docx_template_name or not default_context_name:
         return
-    default_context = _default_context(default_context_name)
+    default_context = get_form_context(default_context_name)
     # vet against default context keys
     user_input_vetted = {k: v for k, v in data.iteritems() if k in default_context and v}
     context = dict(default_context)
