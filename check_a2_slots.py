3b9ba6277cfb85db850358b749936496c7ab43db
Inessa Vasilevskaya <ivasilev@redhat.com>
2022-01-20 23:00:24 +0100

    Real polling and csv output introduced
    A separate requirements-a2checker.txt created

--- check_a2_slots.py
+++ check_a2_slots.py
@@ -1,5 +1,12 @@
+"""
+A helper script to track free A2 Czech language exam slots.
+"""
+
 import argparse
+import csv
+import datetime
 import sys
+import time
 
 from bs4 import BeautifulSoup
 import requests
@@ -7,18 +14,30 @@ import unidecode
 
 
 URL = 'https://cestina-pro-cizince.cz/trvaly-pobyt/a2/online-prihlaska/'
+# interval to wait before repeating the request
+POLLING_INTERVAL = 15
+CSV_FILENAME = 'out.csv'
 
 
 def _get_data(url=URL, tag='div', cls='town'):
-    resp = requests.get(url)
+    try:
+        resp = requests.get(url, headers={'Cache-Control': 'no-cache',
+                                          'Pragma': 'no-cache',
+                                          'User-agent': 'Mozilla/5.0'})
+    except requests.exceptions.ConnectionError:
+        return []
     if resp.ok:
         html = resp.text
-    soup = BeautifulSoup(html, features="lxml")
-    return [e.text.split() for e in soup.find_all(tag) if getattr(e, tag) and cls in getattr(e, tag)["class"]]
+        soup = BeautifulSoup(html, features="lxml")
+        return [e.text.split() for e in soup.find_all(tag) if getattr(e, tag) and cls in getattr(e, tag)["class"]]
+    return []
 
 
-def get_schools(fetched_data=_get_data()):
+def get_schools(fetched_data=None):
+    if not fetched_data:
+        fetched_data = _get_data()
     res = {}
+    timestamp = datetime.datetime.now().timestamp()
     # Sometimes the name of a town consists of several words, account for that
     for city_info in fetched_data:
         not_a_name_num, _ = next(((i, w) for (i, w) in enumerate(city_info) if w.startswith('(')), None)
@@ -30,32 +49,77 @@ def get_schools(fetched_data=_get_data()):
         res[city_name_no_diacrytics] = {'free_slots': free_slots,
                                         'total_schools': total_schools,
                                         'status': status,
-                                        'city_name': city_name}
+                                        'city_name': city_name,
+                                        'timestamp': timestamp}
     return res
 
 
-def check_city(city, schools):
+# XXX FIXME Has to go after debugging part done
+def pprint_city(city, schools, prev_schools=None):
     no_diacrytics_city = unidecode.unidecode(city)
-    if no_diacrytics_city not in schools:
-        return sys.exit(f'No exams take place in {city}')
     city_czech_name = schools[no_diacrytics_city]['city_name']
-    res = (f'Congratulations, there are free slots in {city_czech_name} :)'
-           if schools[no_diacrytics_city]['free_slots'] else
-           f'Try again later, no free slots in {city_czech_name} :(')
+    date = datetime.datetime.fromtimestamp(schools[no_diacrytics_city]['timestamp']).strftime('"%m/%d/%Y, %H:%M:%S"')
+    if not prev_schools:
+        # No information to compare with - provide data as is
+        if no_diacrytics_city not in schools:
+            sys.exit(f'No exams take place in {city}')
+        res = (f'Congratulations, there are free slots in {city_czech_name} :)'
+               if schools[no_diacrytics_city]['free_slots'] else
+               f'Try again later, no free slots in {city_czech_name} :(')
+    else:
+        # Previous state is provided - keep track of changes only
+        if schools[no_diacrytics_city]['free_slots'] != prev_schools[no_diacrytics_city]['free_slots']:
+            res = (f'[{date}] Oops, no more free slots in {city_czech_name} :('
+                   if not schools[no_diacrytics_city]['free_slots'] else
+                   f'[{date}] Quick, book your exam in {city_czech_name}! :)')
     print(res)
-    return schools[no_diacrytics_city]
 
 
-def parse_args(args, cities):
+def write_csv(schools, tracked_cities, filename=CSV_FILENAME):
+    with open(filename, 'a', newline='') as csvfile:
+        fieldnames = ['timestamp', 'free_slots', 'city']
+        writer = csv.DictWriter(csvfile, fieldnames)
+        for city in tracked_cities:
+            no_diacrytics_city = unidecode.unidecode(city)
+            writer.writerow({'timestamp': schools[no_diacrytics_city]['timestamp'],
+                             'free_slots': schools[no_diacrytics_city]['free_slots'],
+                             'city': schools[no_diacrytics_city]['city_name']})
+
+
+def parse_args(args, cities_choices):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--city', help='City to track exams in', choices=cities, action='append')
+    parser.add_argument('--city', help='City to track exams in', choices=cities_choices, action='append')
+    parser.add_argument('--out', help='File to store json with schools data', default=CSV_FILENAME)
     return parser.parse_args(args)
 
 
 if __name__ == "__main__":
-    all_schools = get_schools()
-    all_cities = sorted(c for c in all_schools)
-    parsed_args = parse_args(sys.argv[1:], all_cities)
-    cities = parsed_args.city or all_cities
-    for a_city in cities:
-        check_city(a_city, all_schools)
+    try:
+        all_schools = get_schools()
+        all_cities = sorted(c for c in all_schools)
+        parsed_args = parse_args(sys.argv[1:], all_cities)
+        cities = parsed_args.city or all_cities
+        # show details for all cities
+        for a_city in all_cities:
+            pprint_city(a_city, all_schools)
+        print(f"Will be tracking status in {cities}")
+        write_csv(all_schools, cities, filename=parsed_args.out)
+
+        old_data = all_schools
+        while True:
+            time.sleep(POLLING_INTERVAL)
+            new_data = get_schools()
+            if not new_data:
+                # possibly not happy about scraper, wait and retry
+                print("Looks like connection error, will try again later")
+                continue
+            print(f"Fetched data, available slots in {[c for c in new_data if new_data[c]['free_slots']]}")
+            if any(old_data[c]['free_slots'] != new_data[c]['free_slots'] for c in cities):
+                # show details for tracked cities
+                for a_city in cities:
+                    pprint_city(a_city, new_data, old_data)
+                # update data
+                write_csv(new_data, cities, filename=parsed_args.out)
+                old_data = new_data
+    except KeyboardInterrupt:
+        sys.exit('Interrupted by user.')
