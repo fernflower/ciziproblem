83a87222d562da0aca706872062441fd0229560a
Inessa Vasilevskaya <ivasilev@redhat.com>
2022-01-22 12:48:10 +0100

    Refactor checker using asyncio
    Future plans are to combine telegram bot and checker together,
    imho using coroutines is a way to go.

--- check_a2_slots.py
+++ check_a2_slots.py
@@ -1,45 +1,43 @@
-"""
-A helper script to track free A2 Czech language exam slots.
-"""
-
 import argparse
+import asyncio
 import csv
 import datetime
+import random
 import sys
-import time
 
 from bs4 import BeautifulSoup
 import requests
 import unidecode
 
-
 URL = 'https://cestina-pro-cizince.cz/trvaly-pobyt/a2/online-prihlaska/'
 # interval to wait before repeating the request
 POLLING_INTERVAL = 15
 CSV_FILENAME = 'out.csv'
+LAST_FETCHED = 'last_fetched.html'
 
 
-def _get_data(url=URL, tag='div', cls='town'):
+async def _do_fetch(url):
     try:
         resp = requests.get(url, headers={'Cache-Control': 'no-cache',
                                           'Pragma': 'no-cache',
                                           'User-agent': 'Mozilla/5.0'})
     except requests.exceptions.ConnectionError:
-        return []
+        return
     if resp.ok:
-        html = resp.text
-        soup = BeautifulSoup(html, features="lxml")
-        return [e.text.split() for e in soup.find_all(tag) if getattr(e, tag) and cls in getattr(e, tag)["class"]]
-    return []
+        return resp.text
 
 
-def get_schools(fetched_data=None):
-    if not fetched_data:
-        fetched_data = _get_data()
+def _html_to_list(html, tag, cls):
+    """
+    In case layout changes this function only has to be tuned to extract necessary data.
+    Returned value is a dict with no-diacrytics-city-name used as keys
+    """
     res = {}
     timestamp = datetime.datetime.now().timestamp()
+    soup = BeautifulSoup(html, features="lxml")
+    schools_data = [e.text.split() for e in soup.find_all(tag) if getattr(e, tag) and cls in getattr(e, tag)["class"]]
     # Sometimes the name of a town consists of several words, account for that
-    for city_info in fetched_data:
+    for city_info in schools_data:
         not_a_name_num, _ = next(((i, w) for (i, w) in enumerate(city_info) if w.startswith('(')), None)
         city_name = ' '.join(city_info[0: not_a_name_num])
         total_schools = int(city_info[not_a_name_num].lstrip('('))
@@ -52,6 +50,37 @@ def get_schools(fetched_data=None):
                                         'city_name': city_name,
                                         'timestamp': timestamp}
     return res
+
+
+async def fetch(url, filename=None):
+    res = await _do_fetch(url=url)
+    while not res:
+        print("Looks like connection error, will try again later")
+        await asyncio.sleep(random.randint(1, POLLING_INTERVAL))
+        res = await _do_fetch(url=url)
+    # record new data
+    if filename:
+        with open(filename, 'w') as f:
+            f.write(res)
+    return res
+
+
+def get_schools_from_file(filename=LAST_FETCHED, tag='div', cls='town'):
+    with open(filename) as f:
+        html = f.read()
+    return _html_to_list(html, tag, cls)
+
+
+async def fetch_schools(url, filename=LAST_FETCHED, tag='div', cls='town'):
+    html = await fetch(url, filename=filename)
+    return _html_to_list(html, tag=tag, cls=cls)
+
+
+def parse_args(args, cities_choices):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--city', help='City to track exams in', choices=cities_choices, action='append')
+    parser.add_argument('--out', help='File to store csv with schools data change over time', default=CSV_FILENAME)
+    return parser.parse_args(args)
 
 
 # XXX FIXME Has to go after debugging part done
@@ -86,36 +115,24 @@ def write_csv(schools, tracked_cities, filename=CSV_FILENAME):
                              'city': schools[no_diacrytics_city]['city_name']})
 
 
-def parse_args(args, cities_choices):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--city', help='City to track exams in', choices=cities_choices, action='append')
-    parser.add_argument('--out', help='File to store json with schools data', default=CSV_FILENAME)
-    return parser.parse_args(args)
-
-
-if __name__ == "__main__":
+async def main():
+    # fetch initial data to set everything up (default choices for cities etc)
+    schools = await fetch_schools(url=URL)
+    all_cities = sorted(schools.keys())
+    parsed_args = parse_args(sys.argv[1:], cities_choices=all_cities)
+    cities = parsed_args.city or all_cities
+    old_data = schools
+    # XXX FIXME DEBUG show details for tracked cities
+    for a_city in cities:
+        pprint_city(a_city, old_data)
     try:
-        all_schools = get_schools()
-        all_cities = sorted(c for c in all_schools)
-        parsed_args = parse_args(sys.argv[1:], all_cities)
-        cities = parsed_args.city or all_cities
-        # show details for all cities
-        for a_city in all_cities:
-            pprint_city(a_city, all_schools)
-        print(f"Will be tracking status in {cities}")
-        write_csv(all_schools, cities, filename=parsed_args.out)
-
-        old_data = all_schools
         while True:
-            time.sleep(POLLING_INTERVAL)
-            new_data = get_schools()
-            if not new_data:
-                # possibly not happy about scraper, wait and retry
-                print("Looks like connection error, will try again later")
-                continue
+            await asyncio.sleep(POLLING_INTERVAL)
+            new_data = await fetch_schools(url=URL)
+            # XXX FIXME DEBUG
             print(f"Fetched data, available slots in {[c for c in new_data if new_data[c]['free_slots']]}")
             if any(old_data[c]['free_slots'] != new_data[c]['free_slots'] for c in cities):
-                # show details for tracked cities
+                # XXX FIXME DEBUG show details for tracked cities
                 for a_city in cities:
                     pprint_city(a_city, new_data, old_data)
                 # update data
@@ -123,3 +140,7 @@ if __name__ == "__main__":
                 old_data = new_data
     except KeyboardInterrupt:
         sys.exit('Interrupted by user.')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
