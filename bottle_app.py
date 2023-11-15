20fbc9f710abaca0c9e96856a5585cf22b62fa1a
Inessa Vasilevskaya <ivasilev@redhat.com>
2023-11-15 23:46:47 +0100

    Add fetcher_status endpoint
    The endpoint can be used to update fetcher status and
    get information about current state.

--- bottle_app.py
+++ bottle_app.py
@@ -181,8 +181,10 @@ def get_exams_info(filepath):
 
 
 @post('/trvaly-pobyt/a2/<filepath>')
-def post_exams_info(filepath, update_time_file='lastupdate'):
+def post_exams_info(filepath):
     token = request.forms.token
+    # Create ts file by default only if saving html. Json will have ts in the data
+    update_time_file = 'lastupdate' if filepath == 'online-prihlaska' else request.forms.update_time_file
     if not token or token != TOKEN_POST:
         # token mismatch, show petition page instead
         return redirect(TOKEN_MISMATCH_REDIRECT_URL)
@@ -195,6 +197,30 @@ def post_exams_info(filepath, update_time_file='lastupdate'):
         date = datetime.datetime.now().timestamp()
         f.write(str(date))
     return static_file(filepath, root=EXAMS_DATAFILE_ROOT)
+
+
+@post('/trvaly-pobyt/a2/fetcher_status')
+def update_fetcher_status():
+    filepath = 'fetcher_status'
+    token = request.forms.token
+    if not token or token != TOKEN_POST:
+        # token mismatch, show petition page instead
+        return redirect(TOKEN_MISMATCH_REDIRECT_URL)
+    # if token is ok -> update data and show stored data
+    status_file = os.path.join(EXAMS_DATAFILE_ROOT, filepath)
+    if not os.path.isfile(status_file):
+        current_status = {}
+    else:
+        with open(os.path.join(EXAMS_DATAFILE_ROOT, filepath)) as f:
+            try:
+                current_status = json.loads(f.read())
+            except json.JSONDecodeError:
+                current_status = {}
+    # Now update status based on info from request
+    current_status[request.forms.id] = request.forms.status
+    with open(os.path.join(EXAMS_DATAFILE_ROOT, filepath), 'w') as f:
+        f.write(json.dumps(current_status))
+    return static_file(filepath, root='data/files/trvalypobytexamchecker/')
 
 
 @route('/')
